1bec0fbfab4ea3af7f6f9daf3e807f24fa5caa85
gabriela.moleta@aluno.senai.br <gabriela.moleta@aluno.senai.br>
2025-04-15 21:37:56 -0300

    feat: criação de heroSection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 import Footer from "@/components/footer";
 import styles from "./page.module.css";
 import Header from "@/components/header";
+import HeroSection from "@/components/heroSection";
 
 export default function Home() {
-
   return (
     <div className={styles.container}>
       <Header
@@ -16,6 +16,22 @@ export default function Home() {
           { label: "Sobre", href: "/about" },
         ]}
       />
+
+      <div className={styles.mainContent}>
+        <div className={styles.contentArea}>
+          <HeroSection
+            title={"Meme do Dia: Quando seu código funciona de primeira"}
+            description={
+              "Este momento raro que todos os desenvolvedores sonham em experimentar!"
+            }
+            image={"https://i.imgur.com/vldGJnz.jpg"}
+            likes={2452}
+            comments={387}
+            author={"SuperCoder"}
+            authorAvatar={"https://i.pravatar.cc/150?img=20"}
+          />
+        </div>
+      </div>
 
       <Footer
         footerData={[
